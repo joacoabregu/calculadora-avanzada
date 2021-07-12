b8d59a69f21fc7687ef9a66d6db4f7a6039e71f5
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-12 17:28:11 -0300

    config inicial

---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+
+function Display() {
+    return (
+        <>
+            <input type="number" value={0} />
+            <button type="button">Clear</button>
+        </>
+    )
+}
+
+export default Display;
