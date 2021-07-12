bdffb094f80f19960dbedefa8f0dff23a02f7fbe
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-12 20:01:58 -0300

    added Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import './App.css';
 import Display from './components/Display';
 import Calculations from './components/Calculations';
+import {connect} from 'react-redux';
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Calculadora Avanzada</h1>
       </header>
-      <Display />
-      <Display />
+      <Display value={props.valueOne}/>
+      <Display value={props.valueTwo}/>
       <Calculations />
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+      valueOne: state.valueOne,
+      valueTwo: state.valueTwo,
+      add: state.add,
+      substract: state.substract,
+      multiply: state.multiply,
+      divide: state.divide,
+      pow: state.pow,
+      hypot: state.hypot,
+  }
+}
+
+export default  connect(mapStateToProps)(App);
