a11daf0099d6565cbf5e7191461e61a29d68ae9c
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-12 22:18:41 -0300

    added dispatch functions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import './App.css';
 import Display from './components/Display';
 import Calculations from './components/Calculations';
 import {connect} from 'react-redux';
-
+import {valueOne, valueTwo} from './state/action';
 
 function App(props) {
+  function handleValueOne(e){
+    props.dispatch(valueOne(e.target.value))
+  }
+  function handleValueTwo(e){
+    props.dispatch(valueTwo(e.target.value))
+  }
+  let results = [
+    {text: 'Suma', value: props.add},
+    {text: 'Resta', value: props.substract},
+    {text: 'Multiplicación', value: props.multiply},
+    {text: 'División', value: props.divide},
+    {text: 'Potencia', value: props.pow},
+    {text: 'Hipotenusa', value: props.hypot}
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <h1>Calculadora Avanzada</h1>
       </header>
-      <Display value={props.valueOne}/>
-      <Display value={props.valueTwo}/>
-      <Calculations />
+      <Display value={props.valueOne} handleValue={handleValueOne}/>
+      <Display value={props.valueTwo} handleValue={handleValueTwo}/>
+      {results.map((result , index) => <Calculations text={result.text} value={result.value}  key={index}/>) }
     </div>
   );
 }
