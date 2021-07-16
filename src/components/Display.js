2265c089f6b822641fe96867b2d30764327be015
Joaquin Abregu <abregujoaquin@gmail.com>
2021-07-16 19:25:38 -0300

    added handleFocus function

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,21 @@
-function Display(props) {  
+import {connect} from 'react-redux';
+import {focus} from './state/action';
+
+function Display(props) { 
+    function handleFocus(e){
+        if(e.target.id === 'Valor Uno'){
+            props.dispatch(focus(1))
+        }else{
+            props.dispatch(focus(2))
+        }
+      } 
     return (
         <div className="display">
             <h2>{props.title}</h2>
-            <input type="text" value={props.value} onChange={props.handleValue} />
+            <input type="text" id={props.title} value={props.value} onChange={props.handleValue} onFocus={handleFocus} />
             <button type="button" className="btn-clear" onClick={props.handleValue}>Clear</button>
         </div>
     )
 }
 
-export default Display;
+export default connect()(Display);
